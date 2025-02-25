b08f2f60570c3355990cc90681b6878633b707fd
Manny singh <msmaninder702.com>
2025-02-25 19:10:03 +0530

    Recipre code updated..

--- src/Component/Recipe/RecipeItem.jsx
+++ src/Component/Recipe/RecipeItem.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react"
 import { useParams } from "react-router"
 
@@ -9,7 +8,9 @@ const RecipeItem = () => {
     const getFetchId = () => {
         fetch(`https://dummyjson.com/recipes/${id}`)
             .then(res => res.json())
-            .then((data) => { setPost({ name: data.name, img: data.image, cuisine: data.cuisine, mealType: data.mealType }) })
+            .then((data) => { 
+                setPost({ name: data.name, img: data.image, cuisine: data.cuisine, mealType: data.mealType })
+            })
     }
 
     useEffect(() => {
@@ -17,34 +18,23 @@ const RecipeItem = () => {
     }, [id])
 
     return (
-        <>
-            <div className="container w-50 my-4" style={{
-                border: "1px solid", border: "1px solid",
-                borderRadius: "10px",
-                padding: "20px",
-                background: "#ddfadd",
-                minHeight:"264px"
-            }}>
-                <div className="row">
-                    <div className="col-5">
-                        <img src={post.img} alt="" height={200} style={{ borderRadius: "50%" }} />
-                    </div>
-                    <div className="col-7">
-                        <h4>Name: {post.name}</h4>
-                        <p className="fw-bold">Cuisine: {post.cuisine}</p>
-                        <ul>
-                            {
-                                post.mealType.map((item, i) => (
-                                    <li key={i}>{item}</li>
-                                ))
-                            }
-                        </ul>
-                    </div>
+        <div className="container w-50 my-4 p-3 rounded" style={{ background: "#ddfadd", minHeight: "264px" }}>
+            <div className="row align-items-center text-center text-md-start">
+                {/* Image Section */}
+                <div className="col-12 col-md-5 mb-3 mb-md-0">
+                    <img src={post.img} alt={post.name} className="img-fluid rounded-circle" style={{ maxHeight: "200px" }} />
                 </div>
 
-            </div >
-
-        </>
+                {/* Text Section */}
+                <div className="col-12 col-md-7">
+                    <h4 className="fw-bold">{post.name}</h4>
+                    <p className="fw-bold">Cuisine: {post.cuisine}</p>
+                    <ul className="list-unstyled">
+                        {post.mealType.map((item, i) => <li key={i}>{item}</li>)}
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
 
